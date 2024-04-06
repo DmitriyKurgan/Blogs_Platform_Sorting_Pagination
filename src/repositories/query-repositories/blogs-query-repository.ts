77aed2f6b9cddaebb1573ce52ba442dfb4d613f9
Dmitry Kurgan <dimakurgan123789@gmail.com>
2024-04-07 02:52:06 +0300

    Code opt

--- src/repositories/query-repositories/blogs-query-repository.ts
+++ src/repositories/query-repositories/blogs-query-repository.ts
@@ -14,6 +14,5 @@ export const BLogMapper = (blog : WithId<BLogType>) : OutputBlogType => {
 }
 
 export async function getAllBlogs(query: any): Promise<any | { error: string }> {
-    debugger
     return getBlogsFromBD(query);
 }
